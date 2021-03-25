84215ba0a4215aba03f86b9f05a87ae8234c9112
Tin-Trung Nguyen <hopthucuatin@gmail.com>
2021-03-25 23:08:21 +0700

    Fix bug

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -4,7 +4,7 @@ import 'tailwindcss/tailwind.css'
 interface IPostProps {
 }
 
-const Posts : React.FC<IAppProps> = (props: IPostProps) => {
+const Posts : React.FC<IPostProps> = (props: IPostProps) => {
   	return <div>
 		Bài viết
   	</div>;
